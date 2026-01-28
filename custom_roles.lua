f9831a4e0e70a82c8b809d333dc9dd6dbd1a48f3
Henrii202 <hedzielnitzki@gmail.com>
2026-01-28 19:34:08 +0100

    Add custom_roles.lua for user asset overrides

---
+++ custom_roles.lua
@@ -0,0 +1,8 @@
+-- Custom overhead overrides
+-- Key = UserId
+-- Value = decal asset id (number or string)
+
+return {
+    [123456789] = "rbxassetid://11111111111111", -- Example User 1
+    [987654321] = "rbxassetid://22222222222222", -- Example User 2
+}
