cde5979d18324c8f9b054c667693bcf732eabb0d
Henrii202 <hedzielnitzki@gmail.com>
2026-01-28 19:41:39 +0100

    Remove trailing comma from user role entry

--- custom_roles.lua
+++ custom_roles.lua
@@ -3,5 +3,5 @@
 -- Value = decal asset id (number or string)
 
 return {
-    [83759969274385] = "rbxassetid://114546308478047", -- Example User 1
+    [83759969274385] = "rbxassetid://114546308478047" -- Example User 1
 }
